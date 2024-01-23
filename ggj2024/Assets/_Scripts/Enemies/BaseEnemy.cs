e15c54154b169b375a57d67e7df1d82a01492666
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2024-01-23 19:44:17 -0300

    TMP

--- ggj2024/Assets/_Scripts/Enemies/BaseEnemy.cs
+++ ggj2024/Assets/_Scripts/Enemies/BaseEnemy.cs
@@ -2,8 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemy : MonoBehaviour
+public class BaseEnemy : MonoBehaviour, IDamageable
 {
+    public Material BaseMaterial => throw new System.NotImplementedException();
+
+    public Material FlashingWhiteMaterial => throw new System.NotImplementedException();
+
+    public SpriteRenderer CharacterSprite => throw new System.NotImplementedException();
+
+    [SerializeField] private SpriteRenderer _characterSprite;
+    [SerializeField] private Material _flashingWhiteMaterial;
+    [SerializeField] private Material _baseMaterial;
+
+    public void Death()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
