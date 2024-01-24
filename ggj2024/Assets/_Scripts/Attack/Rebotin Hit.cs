52208314dc3f162dec74487b4c78dba3074ba487
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2024-01-23 21:36:41 -0300

    Rebotin fxs

--- ggj2024/Assets/_Scripts/Attack/Rebotin Hit.cs
+++ ggj2024/Assets/_Scripts/Attack/Rebotin Hit.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class RebotinHit : MonoBehaviour, IProjectile
 {
     [SerializeField] private float moveSpeed;
-    private Vector2 _moveDirection;
+    [SerializeField] private Vector2 _moveDirection;
+
+    private float _height;
+
+    private void Awake()
+    {
+        _height = GetComponent<Collider2D>().bounds.extents.y;
+    }
     public void Shoot(Vector2 moveDir)
     {
         _moveDirection = moveDir;
@@ -12,5 +19,17 @@ public class RebotinHit : MonoBehaviour, IProjectile
     private void FixedUpdate()
     {
         transform.Translate(_moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position); 
+
+        if (screenPos.y >= Screen.height - 50 || screenPos.y <= 0 + 50)
+        {
+            _moveDirection.y *= -1;
+        }
+
+        if (screenPos.x >= Screen.width - 50 || screenPos.x <= 0 + 50)
+        {
+            _moveDirection.x *= -1;
+        }
     }
 }
