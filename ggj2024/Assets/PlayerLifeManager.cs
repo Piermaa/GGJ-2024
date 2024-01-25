7247c96cf2dce2e87ad6ed28d5df77ce3b9efabb
Folgui12 <folguerasbenjamin@gmail.com>
2024-01-25 18:01:40 -0300

    Commit

---
+++ ggj2024/Assets/PlayerLifeManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLifeManager : MonoBehaviour
+{
+    public Image LifeBar;
+
+    public int LifeAmount;
+
+    public BaseCharacter PlayerRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRef = GameObject.FindWithTag("Player").GetComponent<BaseCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LifeBar.fillAmount = 1.0f / PlayerRef.CurrentHealth;
+        LocateAndFollowPlayer();
+        Debug.Log(LifeBar.fillAmount);
+    }
+
+    private void LocateAndFollowPlayer()
+    {
+        transform.position = PlayerRef.transform.position - new Vector3(0, .5f, 0);
+    }
+}
