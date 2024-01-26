771cf90572e2d3902bf5431e8c6c69e8a2888c8b
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2024-01-26 15:49:52 -0300

    Tile Sounds

---
+++ ggj2024/Assets/_Scripts/TileSounds.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+[System.Serializable]
+public struct TileCategory
+{
+    public string Category;
+    public Tile[] tiles;
+}
+public class TileSounds : MonoBehaviour
+{
+    [SerializeField] private TileCategory[] tileCategories;
+    [SerializeField] private string defaultCategory = "GrassDefault";
+    private Tilemap tilemap;
+    private Vector3Int location;
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SetTileSound", .1f, .2f);
+    }
+
+    private void SetTileSound()
+    {
+        print(GetTileCategoty());
+    }
+
+    private string GetTileCategoty()
+    {
+        var currentTile = GetCurrentTile();
+
+        string category = string.Empty;
+
+        foreach (var tileCategory in tileCategories) 
+        {
+            foreach (var tile in tileCategory.tiles)
+            {
+                if (tile==currentTile)
+                {
+                    category = tileCategory.Category;
+                }
+            }
+        }
+
+        if (category == string.Empty)
+        {
+            return defaultCategory;
+        }
+        else
+        {
+            return category;
+        }
+    }
+
+    private Tile GetCurrentTile()
+    {
+        location = tilemap.WorldToCell(playerTransform.position);
+
+        return tilemap.GetTile<Tile>(location);
+    }
+}
